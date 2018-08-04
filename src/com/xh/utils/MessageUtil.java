ce3e6b5242de56e2e429ee98c9aa98b8d7174781
liuhuiiang <825378291@qq.com>
2018-08-05 01:46:40 +0800

    消息提醒工具

---
+++ src/com/xh/utils/MessageUtil.java
@@ -0,0 +1,9 @@
+package com.xh.utils;
+
+import com.intellij.openapi.ui.Messages;
+
+public class MessageUtil {
+    public static void err(String msg){
+        Messages.showMessageDialog(msg,"错误",null);
+
+    }}
