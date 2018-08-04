cda78c97ea86c1cbcbc64b5705c9d9be9ee84f18
liuhuiiang <825378291@qq.com>
2018-08-05 01:42:04 +0800

    增加布局文件解析实体

---
+++ src/com/xh/entity/XmlEntity.java
@@ -0,0 +1,11 @@
+package com.xh.entity;
+
+public class XmlEntity {
+    public String type;//类型
+    public String packageName;//包名
+    public String field;//字段名
+    public boolean click=false;//是否点击
+    public String method;//点击回调名字
+    public String id;
+    public boolean use=true;
+}
